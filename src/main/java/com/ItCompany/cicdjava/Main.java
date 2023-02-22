a3476b9a2221e56c0cb342c1bda1762dc42d516a
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-02-22 15:09:50 +0400

    added test for message method

---
+++ src/main/java/com/ItCompany/cicdjava/Main.java
@@ -0,0 +1,12 @@
+package com.ItCompany.cicdjava;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class Main {
+
+    public String message() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/ItCompany/cicdjava/MainTest.java
@@ -0,0 +1,23 @@
+package com.ItCompany.cicdjava;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+class MainTest {
+
+    Main main = new Main();
+
+    @Test
+    public void message_success() {
+        String excepted = "Hello";
+
+        String actual = main.message();
+
+        assertEquals(excepted, actual);
+    }
+}
